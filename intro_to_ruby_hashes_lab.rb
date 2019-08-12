a96542d47596d89d2d8856a08dd1c65d3080ac7d
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 04:38:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # complexity.
   hash = {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {:one => "", :two => "", :three => "", :four => ""},
+    :rent_in_dollars => {:one_piece_owned => 25, :two => "", :three => "", :four => ""},
     :names => {:one => "", :two => "", :three => "", :four => ""}
   }}
 end
